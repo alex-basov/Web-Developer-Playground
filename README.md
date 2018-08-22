0c785c4ccb4d63685f74cb7b09fb4a5ce0fbd924
Alex Basov <bolex.ua@gmail.com>
2018-08-22 13:43:23 +0300

    add scorekeeper app files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# Web Dev Playground" 
+"# Web Dev Playground"
+
+- Score Keeper
---
+++ scorekeeper/scorekeeper.js
@@ -0,0 +1,47 @@
+let player1Button = document.querySelector('#player1');
+let player2Button = document.querySelector('#player2');
+let player1Display = document.querySelector('#player1Display');
+let player2Display = document.querySelector('#player2Display');
+let resetButton = document.getElementById('reset');
+let numberInput = document.querySelector('input');
+let paragraph = document.querySelector('p');
+let player1Score = 0;
+let player2Score = 0;
+let gameOver = false;
+let winningScore = 5;
+
+player1Button.addEventListener('click', function() {
+  if (!gameOver) {
+    player1Score++;
+    if (player1Score === winningScore) {
+      player1Display.classList.add('winner');
+      gameOver = true;
+    }
+    player1Display.textContent = player1Score;
+  }
+});
+
+player2Button.addEventListener('click', function() {
+  if (!gameOver) {
+    player2Score++;
+    if (player2Score === winningScore) {
+      player2Display.classList.add('winner');
+      gameOver = true;
+    }
+    player2Display.textContent = player2Score;
+  }
+});
+
+resetButton.addEventListener('click', function() {
+  player1Score = 0;
+  player2Score = 0;
+  player1Display.textContent = 0;
+  player2Display.textContent = 0;
+  player1Display.classList.remove('winner');
+  player2Display.classList.remove('winner');
+  gameOver = false;
+});
+
+numberInput.addEventListener('change', function() {
+  paragraph.textContent = 'Value changed';
+});
