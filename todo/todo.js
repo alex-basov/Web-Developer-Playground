4ba6765b5f5ca32d29d7411b87be7f2844e2b48b
Alex Basov <bolex.ua@gmail.com>
2018-08-22 15:35:59 +0300

    use for..of in todo list

--- todo/todo.js
+++ todo/todo.js
@@ -1,15 +1,15 @@
-let lis = document.querySelectorAll('li');
+const lis = document.querySelectorAll('li');
 
-for (var i = 0; i < lis.length; i++) {
-  lis[i].addEventListener('mouseover', function() {
+for (let li of lis) {
+  li.addEventListener('mouseover', function() {
     this.classList.add('selected');
   });
 
-  lis[i].addEventListener('mouseout', function() {
+  li.addEventListener('mouseout', function() {
     this.classList.remove('selected');
   });
 
-  lis[i].addEventListener('click', function() {
+  li.addEventListener('click', function() {
     this.classList.toggle('done');
   });
 }
