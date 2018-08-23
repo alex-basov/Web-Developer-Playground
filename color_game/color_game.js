f310e573ce905af1403a48338c46db0cb9ddbb24
Alex Basov <bolex.ua@gmail.com>
2018-08-23 14:33:56 +0300

    add random generated colors and the number of generated colors

--- color_game/color_game.js
+++ color_game/color_game.js
@@ -1,16 +1,9 @@
-const colors = [
-  'rgb(255, 0, 0)',
-  'rgb(255, 255, 0)',
-  'rgb(0, 255, 0)',
-  'rgb(0, 255, 255)',
-  'rgb(0, 0, 255)',
-  'rgb(255, 0, 255)'
-];
-
+const colors = generateRandomColors(6);
 const squares = document.querySelectorAll('.square');
 const colorDisplay = document.getElementById('colorDisplay');
 const messageDisplay = document.getElementById('message');
 const pickedColor = pickColor();
+const h1 = document.querySelector('h1');
 
 colorDisplay.textContent = pickedColor;
 
@@ -25,6 +18,7 @@ for (let i = 0; i < squares.length; i++) {
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = 'Correct!';
       changeColors(clickedColor);
+      h1.style.backgroundColor = pickedColor;
     } else {
       this.style.backgroundColor = '#232323';
       messageDisplay.textContent = 'Try again';
@@ -43,4 +37,27 @@ function changeColors(color) {
 function pickColor() {
   var random = Math.floor(Math.random() * colors.length);
   return colors[random];
+}
+
+function generateRandomColors(num) {
+  // make an array
+  let array = [];
+  // repeat num times
+  for (var i = 0; i < num; i++) {
+    // get random color and push into array
+    array.push(randomColor());
+  }
+  // return that array
+  return array;
+}
+
+function randomColor() {
+  // pick a 'red' from 0 to 255
+  let red = Math.floor(Math.random() * 256);
+  // pick a 'green' from 0 to 255
+  let green = Math.floor(Math.random() * 256);
+  // pick a 'blue' from 0 to 255
+  let blue = Math.floor(Math.random() * 256);
+  // rgb (red, green, blue);
+  return 'rgb(' + red + ', ' + green + ', ' + blue + ')';
 }
