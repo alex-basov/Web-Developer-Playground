27c1e0dae56289bc21f37b4bcf62a165a56b09de
Alex Basov <bolex.ua@gmail.com>
2020-01-18 23:57:35 +0200

    add functions to create deck and nex card functions

--- tasks/blackjack/blackjack.js
+++ tasks/blackjack/blackjack.js
@@ -2,6 +2,8 @@
 // Blackjack game
 //
 
+console.clear();
+
 let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
 let values = [
   'Ace',
@@ -19,14 +21,34 @@ let values = [
   'Two'
 ];
 
-let deck = [];
-
-for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
-  for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
-    deck.push(`${values[valueIdx]} of ${suits[suitIdx]}`);
+function createDeck() {
+  let deck = [];
+  for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+    for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+      deck.push(`${values[valueIdx]} of ${suits[suitIdx]}`);
+    }
   }
+  return deck;
 }
 
+function getNextCard() {
+  return deck.shift(); // the card is removed from the deck
+}
+
+let deck = createDeck();
+
+console.groupCollapsed('The deck');
 for (let i = 0; i < deck.length; i++) {
   console.log(deck[i]);
 }
+console.groupEnd();
+
+let playerCards = [getNextCard(), getNextCard()];
+
+console.group('The game');
+console.log('This is the Blackjack!');
+
+console.log('You are dealt: ');
+console.log('   ' + playerCards[0]);
+console.log('   ' + playerCards[1]);
+console.groupEnd();
