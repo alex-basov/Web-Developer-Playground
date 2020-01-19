476ff51a068dd735102490097fbdf849d1112327
Alex Basov <bolex.ua@gmail.com>
2020-01-19 23:17:59 +0200

    add score calculations

--- tasks/blackjack/blackjack.js
+++ tasks/blackjack/blackjack.js
@@ -64,6 +64,18 @@ newGameBtn.addEventListener('click', () => {
   showStatus();
 });
 
+hitBtn.addEventListener('click', () => {
+  playerCards.push(getNextCard());
+  checkForEndOfGame();
+  showStatus();
+});
+
+stayBtn.addEventListener('click', () => {
+  gameOver = true;
+  checkForEndOfGame();
+  showStatus();
+});
+
 function createDeck() {
   let deck = [];
   for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
@@ -94,14 +106,97 @@ function getCardString(card) {
   return card.value + ' of ' + card.suit;
 }
 
+function getCardNumericValue(card) {
+  switch (card.value) {
+    case 'Ace':
+      return 1;
+    case 'Two':
+      return 2;
+    case 'Three':
+      return 3;
+    case 'Four':
+      return 4;
+    case 'Five':
+      return 5;
+    case 'Six':
+      return 6;
+    case 'Seven':
+      return 7;
+    case 'Eight':
+      return 8;
+    case 'Nine':
+      return 9;
+    default:
+      return 10;
+  }
+}
+
+function getScore(cardArray) {
+  let score = 0;
+  let hasAce = false;
+  for (let i = 0; i < cardArray.length; i++) {
+    let card = cardArray[i];
+    score += getCardNumericValue(card);
+    if (card.value === 'Ace') {
+      hasAce = true;
+    }
+  }
+
+  if (hasAce && score + 10 <= 21) {
+    return score + 10;
+  }
+
+  return score;
+}
+
+function updateScores() {
+  dealerScore = getScore(dealerCards);
+  playerScore = getScore(playerCards);
+}
+
+function checkForEndOfGame() {
+  // TODO
+}
+
 function showStatus() {
   if (!gameStarted) {
     textArea.innerText = 'Welcome to Blackjack!';
     return;
   }
 
-  for (let i = 0; i < deck.length; i++) {
-    textArea.innerText += '\n' + i + ': ' + getCardString(deck[i]);
+  let dealerCardsSting = '';
+  for (let i = 0; i < dealerCards.length; i++) {
+    dealerCardsSting += getCardString(dealerCards[i]) + '\n';
+  }
+
+  let playerCardsSting = '';
+  for (let i = 0; i < playerCards.length; i++) {
+    playerCardsSting += getCardString(playerCards[i]) + '\n';
+  }
+
+  updateScores();
+
+  textArea.innerText =
+    'Dealer has:\n' +
+    dealerCardsSting +
+    '(score: ' +
+    dealerScore +
+    ')\n\n' +
+    'Player has:\n' +
+    playerCardsSting +
+    '(score: ' +
+    playerScore +
+    ')\n\n';
+
+  if (gameOver) {
+    if (playerWon) {
+      textArea.innerText += 'You Win!';
+    } else {
+      textArea.innerText += 'Dealer Wins';
+    }
+
+    showElement(newGameBtn);
+    hideElement(hitBtn, stayBtn);
   }
 }
 
