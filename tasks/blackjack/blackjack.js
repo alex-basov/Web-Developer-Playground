8e00b3a8dea9ff3b6b35ed713241037f05ec246f
Alex Basov <bolex.ua@gmail.com>
2020-01-19 21:37:41 +0200

    add buttons and events to the blackjack game

--- tasks/blackjack/blackjack.js
+++ tasks/blackjack/blackjack.js
@@ -4,8 +4,8 @@
 
 console.clear();
 
-let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
-let values = [
+const suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+const values = [
   'Ace',
   'King',
   'Queen',
@@ -20,6 +20,27 @@ let values = [
   'Three',
   'Two'
 ];
+
+const textArea = document.querySelector('#text-area');
+const newGameBtn = document.querySelector('#new-game-btn');
+const hitBtn = document.querySelector('#hit-btn');
+const stayBtn = document.querySelector('#stay-btn');
+
+hideElement(hitBtn, stayBtn);
+
+newGameBtn.addEventListener('click', () => {
+  textArea.innerText = 'Started...';
+  hideElement(newGameBtn);
+  showElement(hitBtn, stayBtn);
+});
+
+function hideElement(...elements) {
+  elements.forEach(element => (element.style.display = 'none'));
+}
+
+function showElement(...elements) {
+  elements.forEach(element => (element.style.display = 'unset'));
+}
 
 function createDeck() {
   let deck = [];
